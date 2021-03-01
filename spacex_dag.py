d9fe37bab1558aaab56801b32e3ee02a382c9dd2
kdv1406 <79868142+kdv1406@users.noreply.github.com>
2021-03-01 22:36:32 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -18,9 +18,10 @@ dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
 t1 = BashOperator(
     task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
+    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data{}".format(" -r {{ params.rocket }}" if rocket !='all' else ""), 
     dag=dag
 )
+
 
 t2 = BashOperator(
     task_id="print_data", 
