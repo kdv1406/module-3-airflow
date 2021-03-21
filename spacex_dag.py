e63f6b37745c05cd377fcfbe17b79d9e7cada49e
kdv1406 <79868142+kdv1406@users.noreply.github.com>
2021-03-21 22:24:15 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -20,7 +20,7 @@ dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 for i in ["all","falcon1","falcon9","falconheavy"]:
     t1 = BashOperator(
         task_id="get_data_{}".format(i), 
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data {}".format("-r {{params.rocket}}" if i != "all" else ""),  
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{{{ execution_date.year }}}} -o /var/data {}".format("-r {{params.rocket}}" if i != "all" else ""),  
         params={"rocket": i},
         dag=dag
     )
